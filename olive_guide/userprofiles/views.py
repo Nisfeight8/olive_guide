a5cd98b50c8a3d211c752a658824aaa3c3a94e75
Nisfeight8 <fanispapazoglou1999@gmail.com>
2021-12-04 16:53:54 +0200

    ATOG-7 fixed userprofiles app and tests

--- olive_guide/userprofiles/views.py
+++ olive_guide/userprofiles/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
 from django.contrib.auth.forms import PasswordChangeForm
-from .forms import UserUpdateForm
 from django.views.generic import UpdateView,FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
@@ -9,23 +7,29 @@ from django.utils.translation import ugettext_lazy as _
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth import update_session_auth_hash
 
-class UserUpdateView(SuccessMessageMixin,UpdateView,LoginRequiredMixin):
+
+class UserUpdateView(LoginRequiredMixin,SuccessMessageMixin,UpdateView):
     model=User
     fields=('username','email','first_name','last_name',)
     success_url = reverse_lazy('user_profile:profile')
     success_message = _("Your profile updated successfully.")
     template_name = 'user_profile_update.html'
+
     def get_object(self,queryset=None):
         return self.request.user
-class ChangePasswordView(SuccessMessageMixin,FormView,LoginRequiredMixin):
+
+
+class ChangePasswordView(LoginRequiredMixin,SuccessMessageMixin,FormView):
     form_class = PasswordChangeForm
     success_message =  _("Your password changed successfully.")
     success_url = reverse_lazy('user_profile:profile')
     template_name = 'user_change_password.html'
+
     def get_form_kwargs(self):
         kwargs = super(ChangePasswordView, self).get_form_kwargs()
         kwargs['user'] = self.request.user
         return kwargs
+
     def form_valid(self, form):
         user = form.save()
         update_session_auth_hash(self.request, user)  # Important!
